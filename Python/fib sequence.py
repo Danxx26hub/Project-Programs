8c9482945a0ab54e1fda703828a0f60200a5b708
vdrey <daniel7reynolds@gmail.com>
2014-10-05 09:24:16 -0400

    Update fib sequence.py

--- Python/fib sequence.py
+++ Python/fib sequence.py
@@ -1,7 +1,7 @@
 #Enter a number and have the program generate the Fibonacci
 #sequence to that number or to the Nth number.
 
-#where there is a backslash, I need to tab over.
+#Where there is a backslash, I need to tab over.
 
 def nthFib(n):
 /fibTerms = [0, 1]
