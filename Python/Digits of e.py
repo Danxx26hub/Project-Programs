25ac1e08f9351cf4d89d970a4a5cc56f0202a8c1
vdrey <daniel7reynolds@gmail.com>
2014-10-05 10:24:23 -0400

    Update Digits of e.py

--- Python/Digits of e.py
+++ Python/Digits of e.py
@@ -1,4 +1,17 @@
-def eFormula(x):
+#Enter a number and have the program generate e up to that many decimal places.
+
+def eFormula(y):
     
-    n = (1+(1/x))**x
+    n = (1+(1/y))**y
     return n
+
+
+print("How many decimal places?")
+x = input()
+
+a = 100000000000000000000000000000000000
+while len(str(eFormula(a))) < int(x) - 2:
+    a = a + 1
+
+print(eFormula(a))
+
