1c51f84973e6c560cf1d7a4c6bc679737b9c0c81
vdrey <daniel7reynolds@gmail.com>
2014-11-16 16:16:44 -0500

    Made Collatz work
    Stupid implicit conversion.

--- Python/Collatz.py
+++ Python/Collatz.py
@@ -1,17 +1,17 @@
 #Start with a number n > 1. Find the number of steps it takes to reach one using the following process: 
 #If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1.
 
-print("What is n")
+print("What is n?")
 n = input()
 steps = 0
 while n != 1:
   
-  if n % 2 == 0:
-    n = n/2
+  if int(n) % 2 == 0:
+    n = int(n)/2
     steps = steps + 1
     
   else:
-    n = 3n + 1
+    n = 3*int(n) + 1
     steps = steps + 1
     
-print('Steps:' + steps)
+print('Steps:' + str(steps))
