76f8f03666bc2f358c225e8d35c48f1556240e60
vdrey <daniel7reynolds@gmail.com>
2014-11-16 16:55:06 -0500

    Update Tile cost.py

--- Python/Tile cost.py
+++ Python/Tile cost.py
@@ -1,1 +1,15 @@
-placeholder
+#Calculate the total cost of tile it would take to cover a 
+#floor plan of width and height, using a cost entered by the user.
+
+print('What is the width?')
+w = input()
+
+print('What is the height?')
+h = input()
+
+print('What is the cost per unit of area?')
+c = input()
+
+tc = w * h * c
+
+print('Total Cost: ' + str(tc))
