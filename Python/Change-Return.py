05efb6e335d9b2eb0df35cbba573ee7d319d3757
vdrey <daniel7reynolds@gmail.com>
2014-11-16 17:30:24 -0500

    update change return

--- Python/Change-Return.py
+++ Python/Change-Return.py
@@ -1,1 +1,23 @@
-placeholder
+#The user enters a cost and then the amount of money given.
+#The program will figure out the change and the number of quarters, dimes, nickels, pennies needed for the change.
+
+print('What is the cost?')
+cost = input()
+
+print('How much money is given?')
+given = input()
+
+return = float(given) - float(cost)
+
+hundred = 0
+fifty = 0
+twenty = 0
+ten = 0
+five = 0
+one = 0
+quarter = 0 
+dime = 0
+nickel = 0
+penney = 0
+
+while return >= 0:
