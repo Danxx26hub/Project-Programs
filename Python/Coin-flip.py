c9259c4b302ce5e73b7ad42ea26c13e9962c0869
vdrey <daniel7reynolds@gmail.com>
2014-11-16 22:18:29 -0500

    Update Coin-flip

--- Python/Coin-flip.py
+++ Python/Coin-flip.py
@@ -1,1 +1,14 @@
-Place
+#Write some code that simulates flipping a single coin however many times the user decides. 
+#The code should record the outcomes and count the number of tails and heads.
+
+import random
+
+print('How many flips?')
+flips = input()
+
+count = 0
+
+tails = 0
+heads = 0
+
+while int(count) < int(flips):
