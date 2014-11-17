35c98829481c1aae799def5158aba4f0ec985b98
vdrey <daniel7reynolds@gmail.com>
2014-11-16 22:05:23 -0500

    Tax Calculator edits
    It now works.

--- Python/Tax Calculator.py
+++ Python/Tax Calculator.py
@@ -9,9 +9,11 @@ print('What is the tax rate? (in %)')
 
 rate = input()
 
+rate = float(rate) / 100
+
 tax = float(rate) * float(cost)
 
 tc = tax + float(cost)
 
-print('Tax cost: ' + tax)
-print('Total cost: ' + tc)
+print('Tax cost: ' + str(tax))
+print('Total cost: ' + str(tc))
