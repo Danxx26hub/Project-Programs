50c5a4c19357c83056be16620f3421bdaba511e7
vdrey <daniel7reynolds@gmail.com>
2014-11-22 15:23:50 -0500

    Edit City Distance.py

--- Python/City Distance.py
+++ Python/City Distance.py
@@ -1,1 +1,3 @@
-#Calculates the distance between two cities and allows the user to specify #a unit of distance. This program may require finding coordinates for the #cities like latitude and longitude.
+#Calculates the distance between two cities and allows the user to specify
+#a unit of distance. This program may require finding coordinates for the
+#cities like latitude and longitude.
